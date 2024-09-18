13a22d80816eb2cef2eff3b63f3c6799341217fd
Qian Qian <qiqian82@gmail.com>
2024-09-18 12:39:47 +0800

    learn rust

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -3,6 +3,7 @@ use std::boxed::Box;
 use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
 use std::pin::Pin;
+use std::ptr::addr_of;
 use std::sync::atomic::{AtomicU64, Ordering};
 use super::node::Node;
 use once_cell::sync::OnceCell;
@@ -10,7 +11,7 @@ use once_cell::sync::OnceCell;
 pub struct Engine
 {
     // uid -> pointer
-    pub object_registry : HashMap<u64, RefCell<Pin<Box<Node>>>>,
+    pub object_registry : HashMap<u64, Pin<Box<Node>>>,
 }
 
 pub static mut ENGINE_ROOT: OnceCell<Engine> = OnceCell::new();
@@ -22,14 +23,31 @@ pub fn engine_init() {
     }
 }
 
-pub fn next_global_id() -> u64
+pub fn engine_next_global_id() -> u64
 {
     unsafe {
         UID_GENERATOR.fetch_add(1, Ordering::Acquire)
     }
 }
-pub fn pin_object(id : u64, pin: RefCell<Pin<Box<Node>>>) {
+pub fn engine_pin(id : u64, obj: Node) -> *const dyn Any {
+    let pin = Box::pin(obj);
+    let addr = addr_of!(*pin);
     unsafe {
         ENGINE_ROOT.get_mut().unwrap().object_registry.insert(id, pin);
+    }
+    addr
+}
+
+pub fn engine_cast_mut(addr : *const dyn Any) -> &'static mut Node {
+    unsafe {
+        &mut *(addr as *mut Node)
+    }
+}
+
+pub fn engine_remove(addr : *const dyn Any) -> Pin<Box<Node>> {
+    unsafe {
+        let c = &*(addr as *const Node);
+        let cid = c.id;
+        ENGINE_ROOT.get_mut().unwrap().object_registry.remove(&cid).unwrap()
     }
 }
--- engine/src/node.rs
+++ engine/src/node.rs
@@ -1,3 +1,4 @@
+use std::any::Any;
 use std::cell::{RefCell, RefMut};
 use std::collections::HashMap;
 use std::hash::Hash;
@@ -10,38 +11,34 @@ use super::engine::{*};
 
 pub struct Node
 {
-    id: u64,
-    parent: RefCell<Weak<Node>>,
-    children: HashMap<u64, RefCell<Pin<Box<Node>>>>,
+    pub id: u64,
+    parent: Weak<RefCell<Node>>,
+    children: HashMap<u64, Pin<RefCell<Box<Node>>>>,
 }
 
 #[no_mangle]
 pub extern "C"
-fn Node_new() -> u64 {
-    let node_id = next_global_id();
+fn Node_new() -> *const dyn Any {
+    let node_id = engine_next_global_id();
     let node = Node {
         id: node_id,
-        parent: RefCell::new(Weak::new()),
+        parent: Weak::new(),
         children: HashMap::new()
     };
-    let add0 = addr_of!(node);
-
-    let pin = RefCell::new(Box::pin(node));
-
-    let ptr = addr_of!(pin);
-    pin_object(node_id, pin);
-
-    let add1 = ptr as u64;
-    println!("test");
-    add1
+    engine_pin(node_id, node)
 }
 
 #[no_mangle]
 pub extern "C"
-fn Node_add_child(parent: u64, child: u64) {
+fn Node_add_child(parent: *const dyn Any, child: *const dyn Any) {
     unsafe {
-        let p = &*(parent as *const RefCell<Pin<Box<Node>>>);
-        let c = ENGINE_ROOT.get_mut().unwrap().object_registry.remove(&child).unwrap();
-        p.borrow_mut().children.insert(child, c);
+        let p = engine_cast_mut(parent);
+        let c = engine_cast_mut(child);
+        if c.parent.upgrade().is_none() {
+            let c_obj = engine_remove(child);
+            //let x = Rc::downgrade(&Rc::new(Box::new(p)));
+            p.children.insert(c_obj.id, c_obj);
+        }
+        println!();
     }
 }
