797acb04ddf682847305e64aaff059bd250984fb
Qian Qian <qiqian82@gmail.com>
2024-10-09 01:46:41 +0800

    switch to maven mirrors

--- framework.bk/build.gradle.kts
+++ framework.bk/build.gradle.kts
@@ -1,6 +1,9 @@
 allprojects  {
     repositories {
-        mavenCentral()
-        maven("<https://maven.pkg.jetbrains.space/public/p/compose/dev>")
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public") }
+        maven { url = uri("https://repo.huaweicloud.com/repository/maven") }
+        maven { url = uri("https://mirrors.163.com/maven/repository/maven-public") }
     }
 }
--- framework.bk/settings.gradle.kts
+++ framework.bk/settings.gradle.kts
@@ -3,7 +3,12 @@ rootProject.name = "framework"
 pluginManagement {
     resolutionStrategy {
         repositories {
-            gradlePluginPortal()
+            maven { url = uri("https://maven.aliyun.com/repository/google") }
+            maven { url = uri("https://maven.aliyun.com/repository/public") }
+            maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+            maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public") }
+            maven { url = uri("https://repo.huaweicloud.com/repository/maven") }
+            maven { url = uri("https://mirrors.163.com/maven/repository/maven-public") }
         }
     }
 }
